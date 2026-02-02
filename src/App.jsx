a9fcbdf425864d3e3ed271d75d4ba9968a4ea268
kyle10wolfe-byte <kyle10wolfe@gmail.com>
2026-02-02 12:24:21 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import Todo from './components/Todo.jsx';
+import Title from './components/Title.jsx';
+import Modal from './components/Modal.jsx';
+
+function App() {
+  return (
+    <div>
+      <Title />
+      <div className="todo__wrapper">
+        <Todo />
+        <Todo />
+        <Todo />
+      </div>
+      <Modal />
+    </div>
+  );
+}
+
+export default App;
