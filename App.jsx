d41b969e0373818287c8a2db0e2365fe9a7c6a04
kyle10wolfe-byte <kyle10wolfe@gmail.com>
2026-02-04 16:37:22 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Nav from "./components/Nav.jsx";
+import Users from './pages/Users.jsx';
+
+
+function App() {
+  return (
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users/:username" element={<Users />} />
+        </Routes>
+      </Router>
+  );
+}
+
+export default App;
--- Modal.jsx
+++ Modal.jsx
@@ -1,16 +1,16 @@
 import './Modal.css';
 
-function Modal() {
+function Modal({ title, cancelModal, confirmModal }) {
     return (
        <>
             <div className="modal">
-                <p className="modal__title">Are you sure?</p>
+            <p className="modal__title">{title}</p>
                 <div className="modal__buttons">
-                    <button className="btn btn__cancel">Cancel</button>
-                    <button className="btn">Confirm</button>
+                    <button onClick={cancelModal}className="btn btn__cancel">Cancel</button>
+                    <button onClick={(confirmModal)} className="btn">Confirm</button>
                 </div>
             </div>
-            <div className="backdrop"></div>
+            <div className="backdrop" />
         </>
     );
 }
